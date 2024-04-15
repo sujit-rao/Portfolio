b1149cbb0e00a5b8e5e35efd60d3d1a9befd3df4
Sujit Rao <sujitrao867@gmail.com>
2024-04-16 02:11:51 +0530

    intro updated

--- client/src/pages/intro.jsx
+++ client/src/pages/intro.jsx
@@ -36,7 +36,7 @@ export default function intro() {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <h1 className="mb-5 text-5xl font-bold">Hello!</h1>
                         <p className="mb-5"> I'm <span className=' font-bold'>Sujit Rao</span>, a student passionate about development.
 
 Explore my portfolio to see my projects and skills. Let's connect!</p>
