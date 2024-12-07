172c3bb895042e7582072fa140a90bab4871ae53
Sujit Rao <sujitrao867@gmail.com>
2024-12-07 11:03:29 +0530

    New resume and fixed the navbar button

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -24,21 +24,29 @@ function Navbar() {
     window.open('Resume.pdf', '_blank', 'noopener,noreferrer');
   };
 
+
+  const[open,setOpen] = useState(false)
+  const navItem =() => {
+      setOpen(!open)
+  }
+
+
+
   return (
     <div>
       <div className={`navbar fixed z-50 ${sticky ? "sticky-navbar shadow-md bg-stone-950 duration-800 transition-all ease-in-out" : " bg-transparent"}`}>
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div tabIndex={0} onClick={navItem} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li><NavLink to={"/"}>Home</NavLink></li>
+            <ul tabIndex={0} className={` menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${open? '' : 'hidden'}`}>
+              <li><NavLink to={"/"} onClick={navItem}>Home</NavLink></li>
 
 
-              <li><NavLink to={"/about"} >About</NavLink></li>
-              <li><NavLink to={"/profile"}>Portfolio</NavLink></li>
-              <li><NavLink to={"/contact"}>Contact</NavLink></li>
+              <li><NavLink to={"/about"} onClick={navItem} >About</NavLink></li>
+              <li><NavLink to={"/profile"} onClick={navItem}>Portfolio</NavLink></li>
+              <li><NavLink to={"/contact"} onClick={navItem}>Contact</NavLink></li>
             </ul>
           </div>
           <Link to={"/"} className="btn btn-ghost text-xl"> <div className="avatar ">
